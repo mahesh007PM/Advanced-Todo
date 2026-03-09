20bf1eed9fa8c4b951db0b162b1c28c3f1d617b7
mahesh007PM <bornlucy.1@gmail.com>
2026-03-09 18:15:10 +0530

    services completed with models

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Todos } from './components/todos/todos';
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [Todos, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
